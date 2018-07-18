a90fea468d7f123a0e94b6752562efe5e39ea8d3
EZhanG14 <36180809+EZhanG14@users.noreply.github.com>
2018-07-18 14:31:31 -0600

    Update Enemies.java

--- Enemies.java
+++ Enemies.java
@@ -1,1 +1,11 @@
-
+/* ideas for enemies,
+  - pokemon based system
+  - only 2 options: fight or run
+  - run always work
+   - enemies will always have 4 health
+   - player has 5 health
+   - player attack will do 1 or 3 damage depending if they have a sword (see inventory.java)
+   - If player dies (will require player to face 2 enemies at least), then show game over screen where terminal tells user that they are dead
+   and thank them for playing.
+   - use any symbols for enemies that are not being used already
+    - find a way to import the enemies into the map.
