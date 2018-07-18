c22dfa27661005e2fd7de5553367063bbb2ec189
EZhanG14 <36180809+EZhanG14@users.noreply.github.com>
2018-07-18 14:39:53 -0600

    Update Movement.java

--- Movement.java
+++ Movement.java
@@ -1,1 +1,4 @@
-
+/* collision detection (print quote when user hits wall)
+ - dont have to add walls in however (map/world class will have that already)
+  - have the user use WASD keys to move.
+  - 
