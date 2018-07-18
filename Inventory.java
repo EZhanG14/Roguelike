eb250a4cb9f94ead57b604240f7d5973a2d0ebbf
EZhanG14 <36180809+EZhanG14@users.noreply.github.com>
2018-07-18 14:34:20 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,1 +1,5 @@
-
+/* - only item available will be a sword
+  - sword will change how much damage u do ( for enemies.java)
+  - find a way to generate word location on map
+  - print text indicating that the plyer found a sword
+  - preferably use "T" as a symbol to represent sword
