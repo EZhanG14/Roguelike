02f8c874c38176bfa347d36512e5d247b931cc90
EZhanG14 <36180809+EZhanG14@users.noreply.github.com>
2018-07-18 15:01:39 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,5 +1,22 @@
 /* - only item available will be a sword
   - sword will change how much damage u do ( for enemies.java)
-  - find a way to generate word location on map
+  - find a way to generate sword location on map
   - print text indicating that the plyer found a sword
   - preferably use "T" as a symbol to represent sword
+  - use ~ to oepn up "inventory tab"
+*/
+public class Inventory{
+  private char sword = "T";
+  private char inventButton = "~";
+  /*if player movement = sword coordinates{
+  system.out.println("You found a sword")
+  }*/
+  
+  /*if player presses "~"{
+      if player has sword{
+        system.out.println("You have one sword.")
+      }else{
+        system.out.println("You do not have a sword.")
+      }
+  }
+}
