00335f90dc88214dd9a9d838f69505d03ada461b
JohnnyMeng123 <41394774+JohnnyMeng123@users.noreply.github.com>
2018-07-18 18:07:16 -0600

    Add files via upload

---
+++ player.java
@@ -0,0 +1,52 @@
+/**
+ * zhaoning meng
+ * 
+ */
+
+public class player {
+  String name;
+  int power;
+  int health=5;
+
+   public void setname(String n)
+  {
+	this.name=n;
+	
+   }
+
+  public String getname()
+ {
+	return name;
+
+  }
+  public void setpower(int p)
+  {
+	power=p;
+	
+   }
+   public int getpower()
+   {
+	return power;
+   }
+    public void sethealth(int h)
+   {
+	this.health=h;
+    }
+
+   public int gethealth()
+    {
+	return health;
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
