b3b3eddcaa7e6453224a8e3e2326eaceb63a1a6c
JohnnyMeng123 <41394774+JohnnyMeng123@users.noreply.github.com>
2018-07-18 18:26:19 -0600

    Update Core.java

--- Core.java
+++ Core.java
@@ -1,6 +1,19 @@
 package mondayDemo;
+import java.util.Scanner;
 //sorry for the bad documentation everyone!
 public class Core {
+	Scanner myScan=new Scanner(System[] args)
+	{
+		player p1=new player();
+		p1.power=1;
+		System.out.println("Player set your Name");
+		String p1.Name=myScan.nextline();
+		p1.setname(p1.Name);
+//Get Sword Change power to 3	
+		 p1.setpower(3);
+		
+		
+	}
 	public static void main(String[] args) {
 	Map map1 = new Map();
 	
