70a38e50c9f33454428af5d4d13bd0ae8575c5f9
guinn8 <41174494+guinn8@users.noreply.github.com>
2018-07-19 20:26:56 -0600

    Update Core.java

--- Core.java
+++ Core.java
@@ -1,32 +1,93 @@
+
 package mondayDemo;
+
 import java.util.Scanner;
+
 //sorry for the bad documentation everyone!
-public class Core {
-	Scanner myScan=new Scanner(System[] args)
+public class core {
+	private static int posX = 1;
+	private static int posY = 1;
+	static Map map1 = new Map();
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args)  
+	player p1=new player();
+	p1.power=1;
+	System.out.println("Player set your Name");
+	String p1.Name=myScan.nextline();
+	p1.setname(p1.Name);
 	{
-		player p1=new player();
-		p1.power=1;
-		System.out.println("Player set your Name");
-		String p1.Name=myScan.nextline();
-		p1.setname(p1.Name);
-//Get Sword Change power to 3	
-		 p1.setpower(3);
-		
-		
-	}
-	public static void main(String[] args) {
-	Map map1 = new Map();
+	
 	
 	String map = "########" +
-			     "#______#" +
-			     "#______#" +
-			     "#______#" +
-			     "#______#" +
-			     "#______#" +
-			     "#______#" +
+			     "#      #" +
+			     "#      #" +
+			     "#      #" +
+			     "#      #" +
+			     "#      #" +
+			     "#      #" +
 			     "########" ;
-	map1.createMap(map);
-	map1.printMap();
 	
+	map1.createMap(map);
+	//map1.printMap();
+	
+	
+	//map1.printMap();
+	
+	
+	 
+	while (true) { // game loop change later
+		move();
+		map1.printMap();
 	}
+	}
+	
+	
+	static public void move(){
+		
+		String input;
+			
+			System.out.println("Your location is " + posX +" "+ posY);
+			System.out.println("what do u want to do at this time?");
+			
+			input=sc.next();
+			System.out.println(input);
+			
+			if(input.equals("w")){	 
+				if (map1.getPos(posX,posY-1)!='#') {
+					map1.setSpace(posX,posY-1,'O');
+					map1.setSpace(posX,posY,' ');
+				System.out.println("you decided to move up");
+				posY--;
+			}
+			}
+			else if(input.equals("s")) {
+				if (map1.getPos(posX,posY+1)!='#') {
+					map1.setSpace(posX,posY+1,'O');
+					map1.setSpace(posX,posY,' ');
+					posY++;
+				}
+			
+			    System.out.println("you decided to move down");
+			}
+			
+			
+		    else if(input.equals("a")){
+		    	if (map1.getPos(posX-1,posY)!='#') {
+					map1.setSpace(posX-1,posY,'O');
+					map1.setSpace(posX,posY,' ');
+					posX--;
+				}
+		    }
+			
+			else if(input.equals("d")) {
+				if (map1.getPos(posX+1,posY)!='#') {
+					map1.setSpace(posX+1,posY,'O');
+					map1.setSpace(posX,posY,' ');
+					posX++;
+				}
+			}
+			else System.out.println("invalid");
+	}
+
+	
 }
