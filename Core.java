230264a0e76ea2f5053b3bcea3ebb2901b2bc3b6
guinn8 <41174494+guinn8@users.noreply.github.com>
2018-07-21 12:12:25 -0600

    Update Core.java

--- Core.java
+++ Core.java
@@ -1,91 +1,50 @@
 package mondayDemo;
+//known bug: starting a player in a wall bugss everything out 
 
 import java.util.Scanner;
 
 //sorry for the bad documentation everyone!
 public class core {
-	private static int posX = 1;
-	private static int posY = 1;
+	
+	static Movers player= new Movers();
 	static Map map1 = new Map();
-	static Scanner sc = new Scanner(System.in);
-	public static void main(String[] args) {  
+	static String map = "########" +
+					    "#      #" +
+					    "# #    #" +
+					    "#  #   #" +
+					    "# x    #" +
+					    "#      #" +
+					    "#      #" +
+					    "########" ;
 	
-		{
-	
-	
-	String map = "########" +
-			     "#      #" +
-			     "#      #" +
-			     "#      #" +
-			     "#      #" +
-			     "#      #" +
-			     "#      #" +
-			     "########" ;
-	
-	map1.createMap(map);
-	//map1.printMap();
-	
-	
-	//map1.printMap();
-	
-	
-	 
-	while (true) { // game loop change later
-		move();
-		map1.printMap();
-	}
-	
-	}
-	}
-	
-	static public void move(){
-		
+	public static String input() {
 		String input;
-			
-			System.out.println("Your location is " + posX +" "+ posY);
-			System.out.println("what do u want to do at this time?");
-			
-			input=sc.next();
-			System.out.println(input);
-			
-			if(input.equals("w")){	 
-				if (map1.getPos(posX,posY-1)!='#') {
-					map1.setSpace(posX,posY-1,'O');
-					map1.setSpace(posX,posY,' ');
-				System.out.println("you decided to move up");
-				posY--;
-			}
-			}
-			else if(input.equals("s")) {
-				if (map1.getPos(posX,posY+1)!='#') {
-					map1.setSpace(posX,posY+1,'O');
-					map1.setSpace(posX,posY,' ');
-					posY++;
-				}
-			
-			    System.out.println("you decided to move down");
-			}
-			
-			
-		    else if(input.equals("a")){
-		    	if (map1.getPos(posX-1,posY)!='#') {
-					map1.setSpace(posX-1,posY,'O');
-					map1.setSpace(posX,posY,' ');
-					posX--;
-				}
-		    }
-			
-			else if(input.equals("d")) {
-				if (map1.getPos(posX+1,posY)!='#') {
-					map1.setSpace(posX+1,posY,'O');
-					map1.setSpace(posX,posY,' ');
-					posX++;
-				}
-			}
-			else System.out.println("invalid");
+		System.out.println("Your location is " + player.getX() +" "+ player.getY());
+		System.out.println("what do u want to do at this time?");
+		Scanner sc = new Scanner(System.in);
+		input=sc.next();
+		return input;
+		
+		
 	}
 
 	
-}
+	public static void main(String[] args) {  
+		while (true) { // game loop change later
+			String input=input();
+			map1.createMap(map);
+			if (input.equals("w")||input.equals("a")||
+				input.equals("s")||input.equals("d")){
+				player.move(input);
+			}
+			map1.printMap();
+		}
+	
+	}
+	}
+	
+	
+	
+
 	
 
