26e69dbe7de81d39047a1cb5ae97cb3502b89e6e
guinn8 <41174494+guinn8@users.noreply.github.com>
2018-07-21 13:25:30 -0600

    Update Core.java

--- Core.java
+++ Core.java
@@ -1,10 +1,8 @@
-package mondayDemo;
-//known bug: starting a player in a wall bugss everything out 
-
 import java.util.Scanner;
 
 //sorry for the bad documentation everyone!
 public class core {
+	Enemy monster =new Enemy();
 	
 	static player player= new player(1,1);
 	static Map map1 = new Map();
@@ -21,6 +19,8 @@ public class core {
 		String input;
 		System.out.println("Your location is " + player.getX() +" "+ player.getY());
 		System.out.println("what do u want to do at this time?");
+		
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		input=sc.next();
 		return input;
@@ -29,15 +29,17 @@ public class core {
 	}
 
 	
-	public static void main(String[] args) {  
+	public static void main(String[] args) { 
+		map1.createMap(map);
 		while (true) { // game loop change later
 			String input=input();
-			map1.createMap(map);
+			
+			if(input.equals("~")) player.checkInv();
 			if (input.equals("w")||input.equals("a")||
 				input.equals("s")||input.equals("d")){
 				player.move(input);
-		
 			}
+			Enemy.moveEnemy();
 			map1.printMap();
 		}
 	}
@@ -45,10 +47,3 @@ public class core {
 	
 	
 	
-
-	
-	
-	
-
-	
-
