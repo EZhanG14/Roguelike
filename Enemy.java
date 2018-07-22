9532ecfe9ca17c38cba7836515ca7d11a45e2372
wtcsaber <39788785+wtcsaber@users.noreply.github.com>
2018-07-21 19:57:36 -0600

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -3,34 +3,34 @@ package mondayDemo;
 import mondayDemo.Core;
 
 public class Enemy extends Movers{
-		private static int posX ;
-		private static int posY;
-		private static boolean Enemy;
+		
+		
 		private static int EnemyposX=4;
 		private static int EnemyposY=4;
 		private static int randomvalue;
+		
 
 	
 		
 		public static void moveEnemy(){
 			
 			randomvalue= (int)(1+Math.random()*(4-1+1));
-			if (randomvalue==1&&(Core.map1.getPos(EnemyposX-1,EnemyposY)!='#') ) {
+			if (randomvalue==1&&(Core.map1.getPos(EnemyposX-1,EnemyposY)==' ')) {
 				Core.map1.setSpace(EnemyposX-1,EnemyposY,'x');
 				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
 				EnemyposX--;
 				
-			}else if(randomvalue==2&&(Core.map1.getPos(EnemyposX+1,EnemyposY)!='#')) {
+			}else if(randomvalue==2&&(Core.map1.getPos(EnemyposX+1,EnemyposY)==' ')) {
 				Core.map1.setSpace(EnemyposX+1,EnemyposY,'x');
 				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
 				EnemyposX++;
 				
-			}else if(randomvalue==3&&(Core.map1.getPos(EnemyposX,EnemyposY+1)!='#')) {
+			}else if(randomvalue==3&&(Core.map1.getPos(EnemyposX,EnemyposY+1)==' ')) {
 				Core.map1.setSpace(EnemyposX,EnemyposY+1,'x');
 				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
 				EnemyposY++;
 				
-			}else if(randomvalue==4&&(Core.map1.getPos(EnemyposX,EnemyposY-1)!='#')) {
+			}else if(randomvalue==4&&(Core.map1.getPos(EnemyposX,EnemyposY-1)==' ')) {
 				Core.map1.setSpace(EnemyposX,EnemyposY-1,'x');
 				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
 				EnemyposY--;
@@ -40,6 +40,8 @@ public class Enemy extends Movers{
 				Core.map1.setSpace(EnemyposX,EnemyposY,'x');
 				
 			}
+			
+					
 		
 		
 		
@@ -49,11 +51,6 @@ public class Enemy extends Movers{
 		
 		
 		
-		if((posX==EnemyposX&&posY==EnemyposY)==true) {
-			Enemy=true;
-		}
   
 	}
 		}
-	
-
