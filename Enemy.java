d5a4ac317801e7a262e0b8bcf28654650a1437dc
wtcsaber <39788785+wtcsaber@users.noreply.github.com>
2018-07-23 10:24:12 -0600

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -2,59 +2,43 @@ package mondayDemo;
 
 import mondayDemo.Core;
 
-public class Enemy{
-	
+public class Enemy extends Movers{
 		
 		
 		private static int EnemyposX=4;
 		private static int EnemyposY=4;
 		private static int randomvalue;
-		
-		
-	
-		
-		
-
+// int the location of enemy and randomvalue
 	
 		
 		public static void moveEnemy(){
 			
-			
-			if (Player.enemyalive=false) {
-				randomvalue=0;
-			}else {
-				randomvalue=(int)(1+Math.random()*(4));
-			}
-			if (randomvalue==1&&(Core.map1.getPos(EnemyposX-1,EnemyposY)==' ')) {
+			randomvalue= (int)(1+Math.random()*(4-1+1));
+			if (randomvalue==1&&(Core.map1.getPos(EnemyposX-1,EnemyposY)!='#') ) {
 				Core.map1.setSpace(EnemyposX-1,EnemyposY,'x');
 				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
 				EnemyposX--;
 				
-			}else if(randomvalue==2&&(Core.map1.getPos(EnemyposX+1,EnemyposY)==' ')) {
+			}else if(randomvalue==2&&(Core.map1.getPos(EnemyposX+1,EnemyposY)!='#')) {
 				Core.map1.setSpace(EnemyposX+1,EnemyposY,'x');
 				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
 				EnemyposX++;
 				
-			}else if(randomvalue==3&&(Core.map1.getPos(EnemyposX,EnemyposY+1)==' ')) {
+			}else if(randomvalue==3&&(Core.map1.getPos(EnemyposX,EnemyposY+1)!='#')) {
 				Core.map1.setSpace(EnemyposX,EnemyposY+1,'x');
 				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
 				EnemyposY++;
 				
-			}else if(randomvalue==4&&(Core.map1.getPos(EnemyposX,EnemyposY-1)==' ')) {
+			}else if(randomvalue==4&&(Core.map1.getPos(EnemyposX,EnemyposY-1)!='#')) {
 				Core.map1.setSpace(EnemyposX,EnemyposY-1,'x');
 				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
 				EnemyposY--;
 				
-			}else if (randomvalue==0)
-					{
-				Core.map1.setSpace(EnemyposX,EnemyposY,' ');
-					}
+			}
 			else {
 				Core.map1.setSpace(EnemyposX,EnemyposY,'x');
 				
-			}
-			
-					
+			}//use the randomvalue to randomly choose the action of enemy
 		
 		
 		
@@ -67,4 +51,5 @@ public class Enemy{
   
 	}
 		}
+	
 
