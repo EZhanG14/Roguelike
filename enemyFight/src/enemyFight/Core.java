d796dacbe6ef10c8ab8952835c530e770b35b126
guinn8 <41174494+guinn8@users.noreply.github.com>
2018-07-30 10:22:03 -0600

    Update Core.java

--- enemyFight/src/enemyFight/Core.java
+++ enemyFight/src/enemyFight/Core.java
@@ -4,26 +4,18 @@ import javafx.scene.layout.*;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Bounds;
-
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import javafx.scene.input.KeyCode;
 
 /**Haven't found a way to check the boundaries of the window. ~Eric.Z
  * Features that are in here:
  * - Movement with WASD
- * - Sprites changing direction when you move
- * - Proper Collision Detecting ( Wont kill you after player dies)
- * - Game freezes/ends when you die.(Dosen't let you keep moving the invisible sprite)
- * If you want to make the player move, 
- * perhaps use Yuxiaos player movement code from the text only version 
- * and implement that
- * or find some way to make it move normally.
+
  *
  * @author Eric Zhang
- *
+ * @author Gavin Guinn
  */
  
 
@@ -32,19 +24,14 @@ public class Core extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
-   /* private ImageView damageView = new ImageView();
-    private ImageView player = new ImageView();*/
     
-  
-
-
     static Pane layout = new Pane();
     
     private Map map1=new Map();
     private Inventory inventory=new Inventory();
-   
-    
+    private Player player1= new Player();
+    private Enemy enemy1= new Enemy();
+	
 	private int pHealth = 10;
 	private int eHealth = 9;
 	private int chestchose=0;
@@ -53,22 +40,20 @@ public class Core extends Application {
 	private double deltaX=0;
 	private double deltaY=0;
 	
-	static Player player1= new Player();
-	static Enemy enemy1= new Enemy();
-	  
     @Override
     public void start(Stage stage) throws InterruptedException {
     	
     	stage.setTitle("Demo 2 player Fight");
-    	Scene scene = new Scene(layout, 600, 600);
+    	Scene scene = new Scene(layout, 600, 650);
     	stage.setScene(scene);
     
     	map1.createMap("void");
 
-    	
-
     	stage.show();
     	
+    	/**
+    	 * this handles player input
+    	 */
     	scene.setOnKeyPressed(e ->{
 			if(e.getCode()==KeyCode.D) {
 				player1.player.setImage(player1.playerRight);
@@ -94,28 +79,34 @@ public class Core extends Application {
     			player1.player.setImage(player1.playerUp);
     		}
     		
+    		if(e.getCode()==KeyCode.H) {
+    			if(inventory.healthbag.isVisible()==true) {
+    				inventory.phealth=10;
+    				inventory.healthbag.setVisible(false);
+    				System.out.println("pHealth "+ inventory.phealth);
+    			}
+    		}
+    		
     		if(e.getCode()==KeyCode.SPACE) {
     			attack=true;
     		}
     	});
     	
+    	/**
+    	 * this handles the main game loop
+    	 */
     	AnimationTimer animator = new AnimationTimer() {
 			@Override
 			public void handle(long arg0){
-				int i1;
+				//int i1;
 				
 				player1.damageView.setLayoutX(-1000);
 				player1.damageView.setLayoutY(-1000);
 
 				for(int i=0;i<10;i++) {
 			    	if (check(deltaX,deltaY)==true) {
-			            
 			    		player1.player.setLayoutY(player1.player.getLayoutY()+deltaY);
 			    		player1.player.setLayoutX(player1.player.getLayoutX()+deltaX);
-				    	
-				    	i1=(int)(Math.random()-3);
-				    	if(enemy1.enemy.getLayoutY()-i1*deltaX>0&&enemy1.enemy.getLayoutX()-i1*deltaX<600) enemy1.enemy.setLayoutX(enemy1.enemy.getLayoutX()-i1*deltaX);
-				    	if(enemy1.enemy.getLayoutY()-i1*deltaY>0&&enemy1.enemy.getLayoutY()-i1*deltaY<600)enemy1.enemy.setLayoutY(enemy1.enemy.getLayoutY()-i1*deltaY);	
 			    	}
     	        }
 				
@@ -125,7 +116,7 @@ public class Core extends Application {
 	    		if (attack==true) {
 		    		if (player1.player.getImage()==player1.playerUp)	
 		    			player1.damageView.setLayoutX(player1.player.getLayoutX()-20);
-		    		player1.damageView.setLayoutY(player1.player.getLayoutY()-75);
+		    			player1.damageView.setLayoutY(player1.player.getLayoutY()-75);
 		        		
 		    		if (player1.player.getImage()==player1.playerDown) {
 		    			player1.damageView.setLayoutX(player1.player.getLayoutX()-15);
@@ -144,8 +135,10 @@ public class Core extends Application {
 	    			}
 	    		
 	    			if(player1.damageView.getBoundsInParent().intersects(enemy1.enemy.getBoundsInParent())) {
-	
-	    				eHealth -= 2;
+	    				System.out.println("eHealth "+ eHealth);
+	        			System.out.println("pHealth "+ pHealth);
+	        			System.out.println();
+	    				eHealth -= inventory.pdamage;
 	    				if(eHealth <=0) {
 	    			    	
 	        				layout.getChildren().remove(enemy1.enemy);
@@ -159,15 +152,19 @@ public class Core extends Application {
 	    	}	
 	    };animator.start(); 
     }
-
+    
+    /**
+     * check handles the collision detection
+	 * @param xDelt distance to check ahead in the x direction
+	 * @param yDelt distance to precheck in the y direction
+	 * @return false if solid object in the way
+	 */
     public boolean check(double xDelt,double yDelt) {
     	Bounds pBound= player1.player.getBoundsInParent();
     	
     	for( Node object: enemy1.hostileG.getChildren()) {
     		
     		if(object.getBoundsInParent().intersects(pBound.getMinX()+xDelt, pBound.getMinY()+yDelt, pBound.getWidth(), pBound.getHeight())){
-    			
-
     			eHealth -= inventory.getpDamage();
     			pHealth -= 1;
 
@@ -198,8 +195,13 @@ public class Core extends Application {
         		map1.chests.getChildren().remove(chest);
     	        chestchose=inventory.getchestchose();
     	        
-    	        if(chestchose==1)inventory.sworda.setVisible(true);
-    	        if(chestchose==2)inventory.healthbag.setVisible(true);
+    	        if(chestchose==1) {
+    	        	inventory.sworda.setVisible(true);
+    	        	inventory.pdamage=4;
+    	        }
+    	        if(chestchose==2) {
+    	        	inventory.healthbag.setVisible(true);
+    	        }
     	        
     	        return false;
         	} 
